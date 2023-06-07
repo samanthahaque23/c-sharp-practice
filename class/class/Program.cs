337f891b85f21aa0e0e29d204762bcd179a000f4
samantha haque <samanthahaque23@gmail.com>
2023-06-07 16:40:15 +0600

    class

--- class/class/Program.cs
+++ class/class/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Helloo, World!");
+﻿using Classes;
+using System;
+
+namespace Classes
+{
+  public class Person
+    {
+        public string? Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("hi {0}, i am {1}", to, Name);
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+            //static member can be accessible only by the class,not by object
+        }
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var person = new Person();
+        //person.Name = "john";
+        //person.Introduce("sam");
+        var pe = Person.Parse("lisa");
+        pe.Introduce("sam");
+    }
+}
